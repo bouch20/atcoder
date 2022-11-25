f3e529f8f85a84aaaa513d8ef8f8554033fd1402
Yoshitomo Oshiro <y.oshiro@churadata.okinawa>
2022-11-25 10:33:34 +0900

    A問題解いた

---
+++ ABC/278/a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <limits>
+#include <algorithm>
+#include <utility>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+
+using namespace std;
+
+#define repd(i,a,b) for (int i=(a);i<(b);++i)
+#define rep(i,n) repd(i,0,n)
+typedef long long ll;
+typedef pair<int, int> P;
+
+int main(int argc, const char * argv[]) {
+
+    int n,k;
+    cin >> n >> k;
+    vector<int> a(n);
+    rep(i, n) cin >> a[i];
+    repd(i, k, n) {
+        cout << a[i] << ' ';
+    }
+    rep(i, min(k, n)){
+        cout << "0 ";
+    }
+    cout << endl;
+    return 0;
+}
